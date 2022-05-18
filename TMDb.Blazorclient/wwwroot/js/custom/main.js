c97615da658e90f3264e37b74a11a7be59415c81
Tazwinator <taylor_stevenson@yahoo.co.uk>
2022-05-18 18:31:05 +0100

    Improved Carousels by re-making them. Finished T1 Single Movie Page. Finished T1 Homepage Banner. Various outher UI tweaks.

---
+++ TMDb.Blazorclient/wwwroot/js/custom/main.js
@@ -0,0 +1,52 @@
+﻿
+window.sliderFunctions = {
+
+    startMovieRowSlider: function () {
+        $(document).ready(function () {
+
+            $('.owl-carousel.movieRow').owlCarousel({
+                loop: true,
+                margin: 10,
+                nav: true,
+                navText: [
+                    "<i class='fa fa-caret-left'></i>",
+                    "<i class='fa fa-caret-right'></i>"
+                ],
+                responsive: {
+                    0: {
+                        items: 2
+                    },
+                    600: {
+                        items: 4
+                    },
+                    1000: {
+                        items: 5
+                    },
+                    1400: {
+                        items: 7
+                    }
+                }
+            })
+
+        });
+    },
+
+    startStandardSlider: function () {
+        $(document).ready(function () {
+
+            $('.owl-carousel.standardCarousel').owlCarousel({
+                loop: false,
+                margin: 10,
+                nav: true,
+                navText: [
+                    "<i class='fa fa-caret-left'></i>",
+                    "<i class='fa fa-caret-right'></i>"
+                ],
+                items: 1
+
+            })
+
+        })
+    }
+
+}
